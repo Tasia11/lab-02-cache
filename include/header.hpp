83b5e31b8d01f56f89bf6b7eb6f5deffc528106e
Tasia11 <61164926+Tasia11@users.noreply.github.com>
2020-12-04 01:26:24 +0300

    Update header.hpp

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,28 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Anastasiya Osipova anstasiyaosipova2001@gmail.com
 
-#ifndef INCLUDE_HEADER_HPP_
-#define INCLUDE_HEADER_HPP_
+#ifndef INCLUDE_CACHE_EXPLORATION_HPP_
+#define INCLUDE_CACHE_EXPLORATION_HPP_
 
-#endif // INCLUDE_HEADER_HPP_
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Exploration {
+ public:
+  Exploration();
+  void Direct();
+  void Reverse();
+  void Random();
+  friend std::ostream& operator<<(std::ostream& out,
+                                  const Exploration& c);
+
+ private:
+  std::string travel_variant;
+  const std::vector<size_t> cache_size = {64 * 1024, 256 * 1024, 3072 * 1024};
+  std::vector<size_t> buffer;
+  std::vector<double> time;
+};
+
+#endif  // INCLUDE_CACHE_EXPLORATION_HPP_
+
