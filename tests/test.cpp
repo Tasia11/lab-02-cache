c9b3dbc2d57654fd7cfd157a2ee004bd9b18672a
Tasia11 <61164926+Tasia11@users.noreply.github.com>
2020-12-04 01:34:46 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,14 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Anastasiya Osipova anstasiyaosipova2001@gmail.com
 
 #include <gtest/gtest.h>
 
 TEST(Example, EmptyTest) {
+    Exploration cache;
+  cache.Direct();
+  std::cout << cache;
+  cache.Reverse();
+  std::cout << cache;
+  cache.Random();
+  std::cout << cache;
     EXPECT_TRUE(true);
 }
